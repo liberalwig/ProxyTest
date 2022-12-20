2fac1c9cdd8c635de101b8c441f1675d5268059a
최승은 <liberalfilm@gmail.com>
2022-12-20 12:37:48 +0900

    2022-12-20-Tue-12:36

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,7 +5,6 @@ import ui.ProxyPrinter;
 public class Client {
 
     public static void main(String[] args) {
-
         Printer printer = new ProxyPrinter(new ColorPrinter());
         printer.print("Hello");
     }
