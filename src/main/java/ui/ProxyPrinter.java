1de1b68cfd28c79e43dfce64aa2be5df7d35030d
최승은 <liberalfilm@gmail.com>
2022-12-20 12:41:12 +0900

    2022-12-20-Tue-12:38

--- src/main/java/ui/ProxyPrinter.java
+++ src/main/java/ui/ProxyPrinter.java
@@ -4,7 +4,7 @@ public class ProxyPrinter implements Printer { // Printer 가 타깃오브젝트
 
     Printer printer;
 
-    public ProxyPrinter(Printer printer){
+    public ProxyPrinter(Printer printer){ // Client 클래스에서 new ProxyPrinter 객체를 생성하므로 여기에 생성자 만들어둠
         this.printer = printer;
     }
 
